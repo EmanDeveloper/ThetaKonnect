f38083c6068c9fa98149d1e0ed1485c6930bdfad
EmanDeveloper <memankhadim@gmail.com>
2024-12-02 23:44:08 +0500

    change

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-import dotenv from "dotenv";
-import app from "./app.js";
-import DBConnection from "./DB/DbConnect.js";
+import dotenv from "dotenv"
+import app from "./src/app.js";
+import DBConnection from "./src/DB/DbConnect.js";
 
 
 dotenv.config({
